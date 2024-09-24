b9ea84edc1f662908d8d4821c5d6e06020e19280
amdev <56517440+thereisnodeveloper@users.noreply.github.com>
2024-09-24 18:00:00 -0400

    refactor: BREAKING CHANGE - replace magic literals
    - Refactored linkedList function to use a config object (instead of
      magic literals) in traverse
    - Introduced evaluator callback function for condition checking, instead
      of evaluateCondition()
    - Added methodSpecificConfigs object for different method behaviors
    - Refactored several methods to use new traverse structure

--- src/index.js
+++ src/index.js
@@ -19,29 +19,44 @@ function linkedList() {
     return false;
   }
 
-  function traverse(
-    conditionObject,
-    mode = null,
-    currentNode = head,
-    currentIndex = 0,
-    resultString = ''
-  ) {
+  function traverse(config) {
+    let {
+      evaluator,
+      callback = null,
+      currentNode = head,
+      currentIndex = 0,
+      resultString = '',
+      caller = null,
+    } = config;
+    // console.log('config:', config);
+
     // TODO: calculate big O for time & space
     // MAYBE: use loop instead
-
     let stopConditionMet = false;
     const currentValue = currentNode.value;
 
-    stopConditionMet = evaluateCondition(conditionObject, mode, currentIndex, currentValue);
+    const methodSpecificConfigs = {
+      [contains]: { targetProperty: currentValue, callbackOptions: {} },
+      [find]: { targetProperty: currentValue, callbackOptions: {} },
+      [at]: { targetProperty: currentIndex, callbackOptions: {} },
+      [pop]: { targetProperty: currentIndex, callbackOptions: {} },
+      [toString]: { targetProperty: currentIndex, callbackOptions: { resultString, currentNode } },
+      [insertAt]: { targetProperty: currentIndex, callbackOptions: {} },
+      [removeAt]: { targetProperty: currentIndex, callbackOptions: {} },
+    };
+  
+    // console.log('caller:', caller)
+    // console.log('methodSpecificComparisonValue[caller]:', methodSpecificComparisonValue[caller])
 
+    // Defines the 'propertyValue' which is the threshold value for 'targetProperty'. The condition will be true when this value is reached or exceeded.
+    // stopConditionMet = evaluateCondition(conditionObject, callback,
+    // currentIndex, currentValue);
+    stopConditionMet = evaluator(methodSpecificConfigs[caller].targetProperty);
     // BASE CASE
-    if (stopConditionMet === true) {
-      if (mode === 'find') {
-        return currentIndex;
-      }
-      if (mode === 'toString') {
-        return `${resultString}( ${currentNode.value} ) -> ` + ' null';
-        // return resultString  + ' null';
+    if (stopConditionMet) {
+      console.log(`stop condition ${stopConditionMet} met`);
+      if (callback !== null) {
+        return callback(methodSpecificConfigs[caller].callbackOptions);
       }
       return currentNode;
     }
@@ -52,24 +67,32 @@ function linkedList() {
     resultString = resultString.concat(`( ${currentNode.value} )`, '->');
     currentNode = currentNode.next;
 
-    return traverse(conditionObject, mode, currentNode, currentIndex + 1, resultString);
+    return traverse({
+      callback,
+      evaluator,
+      currentNode,
+      currentIndex: currentIndex + 1,
+      resultString,
+      caller,
+    });
   }
 
-  function evaluateCondition(conditionObject, mode, currentIndex, currentValue) {
+  function evaluateCondition(conditionObject, callback, currentIndex, currentValue) {
     // MAYBE: instead of using mode, assign "this" to static variable (which poiints to the caller function)
     let { condition1, condition2 } = conditionObject;
     let meetsCondition = false;
-    switch (mode) {
+
+    switch (callback) {
       case 'at':
       case 'pop':
-      case 'toString':
+      case toStringCallback:
       case 'insertAt':
       case 'removeAt': {
         condition2 = currentIndex;
         break;
       }
       case 'contains':
-      case 'find': {
+      case findCallback: {
         condition2 = currentValue;
         break;
       }
@@ -83,11 +106,13 @@ function linkedList() {
 
   function at(targetIndex) {
     isIndexValid(targetIndex);
+    function atCallback() {}
     const result = traverse({ condition1: targetIndex }, 'at');
     return result;
   }
   function pop() {
     if (size <= 0) throw new Error('Cannot pop, size is 0');
+    function popCallback() {}
     const last = { ...tail };
 
     tail.value = null;
@@ -99,22 +124,42 @@ function linkedList() {
 
     return last;
   }
-  function contains(value) {
-    const currentNode = traverse({ condition1: value }, 'contains');
+  function contains(targetValue) {
+    function containsCallback() {}
+    const currentNode = traverse({ condition1: targetValue }, 'contains');
 
-    return currentNode.value === value;
+    return currentNode.value === targetValue;
   }
 
-  function find(value) {
-    const index = traverse({ condition1: value }, 'find');
-    return Number.isInteger(index) ? index : undefined; // traverse will return currentNode (tail) if nothing is found. If found, it will return an index,
+  function find(targetValue) {
+    function findCallback() {
+      return currentIndex;
+    }
+    const index = traverse({ condition1: targetValue }, findCallback);
+    return Number.isInteger(index) ? index : null; // traverse will return currentNode (tail) if nothing is found. If found, it will return an index,
   }
 
   function toString() {
-    return traverse({ condition1: size - 1 }, 'toString');
+    function toStringCallback(config) {
+      const { resultString, currentNode } = config;
+      console.log('starting toStringCallback');
+      return `${resultString}( ${currentNode.value} ) -> ` + ' null';
+    }
+
+    //! !! needs to be bound just condition 1 (targetProperty), and have
+    //! condition 2 (desiredValue) set later inside `traverse()`
+
+    return traverse({
+      evaluator: createEvaluator(size - 1),
+      callback: toStringCallback,
+      currentNode: head,
+      currentIndex: 0,
+      resultString: '',
+      caller: toString,
+    });
   }
-  function append(value) {
-    const newNodeReference = node(value);
+  function append(targetValue) {
+    const newNodeReference = node(targetValue);
 
     checkIfSizeIs1(newNodeReference);
 
@@ -126,8 +171,8 @@ function linkedList() {
     return newNodeReference;
   }
 
-  function prepend(value) {
-    const newNodeReference = node(value, head);
+  function prepend(targetValue) {
+    const newNodeReference = node(targetValue, head);
 
     checkIfSizeIs1(newNodeReference);
 
@@ -136,36 +181,37 @@ function linkedList() {
     return newNodeReference;
   }
 
-  function insertAt(value, index) {
-    isIndexValid(index);
-
-    if (index === 0) {
-      prepend(value);
+  function insertAt(targetValue, targetIndex) {
+    isIndexValid(targetIndex);
+    function insertAtCallback() {}
+    if (targetIndex === 0) {
+      prepend(targetValue);
       return;
     }
-    if (index === size - 1) {
-      append(value);
+    if (targetIndex === size - 1) {
+      append(targetValue);
       return;
     }
 
-    const insertionPoint = traverse({ condition1: index }, 'insertAt');
-    const newNode = node(value, insertionPoint.next);
+    const insertionPoint = traverse({ condition1: targetIndex }, 'insertAt');
+    const newNode = node(targetValue, insertionPoint.next);
     insertionPoint.next = newNode;
     size += 1;
     return newNode;
   }
 
-  function removeAt(index) {
-    isIndexValid(index);
-    if (index === 0) {
+  function removeAt(targetIndex) {
+    isIndexValid(targetIndex);
+    function removeAtCallback() {}
+    if (targetIndex === 0) {
       aList.pop;
       return;
     }
 
-    const nodeBeforeTarget = traverse({ condition1: index - 1 }, 'removeAt');
+    const nodeBeforeTarget = traverse({ condition1: targetIndex - 1 }, 'removeAt');
     const removalTarget = { ...nodeBeforeTarget.next };
     nodeBeforeTarget.next = nodeBeforeTarget.next.next;
-    if (index === size - 1) {
+    if (targetIndex === size - 1) {
       tail = nodeBeforeTarget;
       console.log('NEW tail:', tail);
     }
@@ -227,28 +273,28 @@ function testLinkedList() {
   console.assert(list.head.value === 0, 'Head should be 0 after prepending');
 
   // Test at
-  console.log('Testing at...');
-  console.assert(list.at(0).value === 0, 'Element at index 0 should be 0');
-  console.assert(list.at(2).value === 2, 'Element at index 2 should be 2');
+  // console.log('Testing at...');
+  // console.assert(list.at(0).value === 0, 'Element at index 0 should be 0');
+  // console.assert(list.at(2).value === 2, 'Element at index 2 should be 2');
 
   // Test pop
-  console.log('Testing pop...');
-  const popped = list.pop();
-  console.assert(popped.value === 3, 'Popped element should be 3');
-  console.assert(list.size === 3, 'Size should be 3 after popping');
-  console.assert(list.tail.value === 2, 'New tail should be 2');
+  // console.log('Testing pop...');
+  // const popped = list.pop();
+  // console.assert(popped.value === 3, 'Popped element should be 3');
+  // console.assert(list.size === 3, 'Size should be 3 after popping');
+  // console.assert(list.tail.value === 2, 'New tail should be 2');
 
   // Test contains and find (if implemented)
-  if (list.contains && list.find) {
-    console.log('Testing contains and find...');
-    console.assert(list.contains(1) === true, 'List should contain 1');
-    console.assert(list.contains(5) === false, 'List should not contain 5');
-    console.assert(list.find(0) === 0, 'Index of 0 should be 0');
-    console.assert(list.find(2) === 2, 'Index of 2 should be 2');
-    console.assert(list.find(5) === null, 'find(5) should return null');
-  } else {
-    console.log('contains and find methods not implemented, skipping tests');
-  }
+  // if (list.contains && list.find) {
+  //   console.log('Testing contains and find...');
+  //   console.assert(list.contains(1) === true, 'List should contain 1');
+  //   console.assert(list.contains(5) === false, 'List should not contain 5');
+  //   console.assert(list.find(0) === 0, 'Index of 0 should be 0');
+  //   console.assert(list.find(2) === 2, 'Index of 2 should be 2');
+  //   console.assert(list.find(5) === null, 'find(5) should return null');
+  // } else {
+  //   console.log('contains and find methods not implemented, skipping tests');
+  // }
 
   // Test toString (if implemented)
   if (list.toString) {
@@ -259,8 +305,37 @@ function testLinkedList() {
     console.log('toString method not implemented, skipping test');
   }
 
-  
   console.log('LinkedList Tests Completed');
+}
+
+/**
+ * Creates an evaluator function for comparing a target property with a given value.
+ *
+ * @param {Object} config - Configuration object for creating the evaluator.
+ * @param {Symbol} config.targetProperty - The name of the property to evaluate.
+ * @param {Symbol} config.propertyValue - The value to compare against the target property.
+ * @returns {Function} A bound evaluator function that can be used for property comparison.
+ *
+ * @example
+ * const config = { targetProperty: size -1};
+ * const evaluator = createEvaluator(config);
+ *
+ * // Later usage (inside traverse() function):
+ * evaluator.bind(null, {propertyValue: currentIndex})
+ *
+ * @note The returned evaluator function is partially bound with the targetProperty.
+ * Additional arguments can be passed when calling the evaluator within traverse().
+ */
+
+function createEvaluator(targetProperty) {
+  // const { targetProperty, propertyValue } = config;
+  return function evaluator(propertyValue) {
+    console.log('targetProperty:', targetProperty);
+    console.log('propertyValue:', propertyValue);
+    return targetProperty === propertyValue;
+  };
+  // ??? need to know if I can bind more arguments when I call it within traverse()
+  // return evaluator.bind(null, { targetProperty });
 }
 
 // Run the tests
